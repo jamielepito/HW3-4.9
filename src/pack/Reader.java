4e333f7a1f99c2fd60c5bcc4c91175945d18dab2
Coco Charles <charlesce@beloit.edu>
2017-10-31 14:38:35 -0500

    Initiated reader class with info from last problem

---
+++ src/pack/Reader.java
@@ -0,0 +1,15 @@
+package pack;
+
+public class Reader extends Thread{
+	private String readThread;
+	
+	Reader(String name){
+		readThread = name; //allow to create instance of thread
+	}
+	
+	public void run(){
+		
+	}
+	
+	
+}
